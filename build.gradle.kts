c95f49d316fcc0e2cfbf5f624c5672cb8218d24a
KamilKurde <kamilbak113@gmail.com>
2022-06-04 19:32:22 +0200

    Added fatJar task

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,3 +23,15 @@ tasks.withType<KotlinCompile> {
 application {
     mainClass.set("BotKt")
 }
+
+tasks.register<Jar>("fatJar")
+{
+    dependsOn.addAll(listOf("compileJava", "compileKotlin", "processResources"))
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+    manifest { attributes(mapOf("Main-Class" to application.mainClass)) }
+    val sourcesMain = sourceSets.main.get()
+    val contents = configurations.runtimeClasspath.get()
+        .map { if (it.isDirectory) it else zipTree(it) } +
+            sourcesMain.output
+    from(contents)
+}
