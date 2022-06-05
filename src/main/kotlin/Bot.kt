50627dc26ce0cab8f6fc002ccc972e46f7301649
KamilKurde <kamilbak113@gmail.com>
2022-06-05 21:18:19 +0200

    Added error instruction

--- src/main/kotlin/Bot.kt
+++ src/main/kotlin/Bot.kt
@@ -18,6 +18,17 @@ suspend fun main() {
 								val (dices, max) = message.split("d").map { parts -> parts.toInt() }
 								roll(it, dices, max)
 							}
+							else -> {
+								it.reply(
+									"""
+									Invalid command format, correct format for rolling is
+									`Roll [numberOfDices]d[numberOfSides]` or `Roll [numberOfSides]` for rolling single dice.
+									Try `Roll 10d100` or `Roll 6`
+									
+									*Note:* The maximum number of sides for a single dice and maximum number of dices in single throw is 120.
+									""".trimIndent()
+								)
+							}
 						}
 					}
 				} catch (e: Exception) {
