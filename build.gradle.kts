ff681843804bc77938ebe77465fc61f914a46a11
KamilKurde <kamilbak113@gmail.com>
2022-08-20 23:18:45 +0200

    Replace fatJar with ShadowJar

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,14 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.7.10"
+    id("com.github.johnrengelman.shadow") version "7.1.2"
     application
 }
 
 group = "com.github.KamilKurde"
-version = "1.4"
+version = "1.5"
 
 repositories {
     mavenCentral()
@@ -25,14 +27,7 @@ application {
     mainClass.set("BotKt")
 }
 
-tasks.register<Jar>("fatJar")
-{
-    dependsOn.addAll(listOf("compileJava", "compileKotlin", "processResources"))
-    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
-    manifest { attributes(mapOf("Main-Class" to application.mainClass)) }
-    val sourcesMain = sourceSets.main.get()
-    val contents = configurations.runtimeClasspath.get()
-        .map { if (it.isDirectory) it else zipTree(it) } +
-            sourcesMain.output
-    from(contents)
+tasks.withType(ShadowJar::class.java){
+    minimize()
+    archiveClassifier.set("")
 }
