e2f401fe5e698c85e289cd3a1285bcf17fa68340
KamilKurde <kamilbak113@gmail.com>
2022-08-21 08:35:51 +0200

    Improve main function readability

--- src/main/kotlin/Bot.kt
+++ src/main/kotlin/Bot.kt
@@ -3,28 +3,25 @@ import com.jessecorbett.diskord.bot.events
 
 private val actions = mutableListOf<Action>()
 
-suspend fun main() {
-	bot(System.getenv("RollBotToken"))
-	{
-		events {
-			onMessageCreate {
-				if (it.author.isBot != true && it.content.startsWith("Roll", ignoreCase = true)) {
-					try {
-						actions += Roll(it.copy(content = it.content.drop(5)), actions.any { roll -> roll.isActive })
-					} catch (e: Exception) {
-						actions += Help(
-							it,
-							this,
-						)
-						logger.error(e.message)
-					}
+suspend fun main(): Unit = bot(System.getenv("RollBotToken")) {
+	events {
+		onMessageCreate {
+			if (it.author.isBot != true && it.content.startsWith("Roll", ignoreCase = true)) {
+				try {
+					actions += Roll(it.copy(content = it.content.drop(5)), actions.any { roll -> roll.isActive })
+				} catch (e: Exception) {
+					actions += Help(
+						it,
+						this,
+					)
+					logger.error(e.message)
 				}
 			}
-			onMessageDelete {
-				actions.filter { roll -> roll.initiatorMessage.id == it.id }.forEach {
-					it.delete()
-					actions -= it
-				}
+		}
+		onMessageDelete {
+			actions.filter { roll -> roll.initiatorMessage.id == it.id }.forEach {
+				it.delete()
+				actions -= it
 			}
 		}
 	}
