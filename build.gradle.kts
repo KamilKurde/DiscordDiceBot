49de86b9ceebbab6e42a0479d7c8762de2bdd7dc
KamilKurde <kamilbak113@gmail.com>
2023-11-06 21:22:24 +0100

    Disable minimizing

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,31 +2,30 @@ import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.9.20"
-    id("com.github.johnrengelman.shadow") version "8.1.1"
-    application
+	kotlin("jvm") version "1.9.20"
+	id("com.github.johnrengelman.shadow") version "8.1.1"
+	application
 }
 
 group = "com.github.KamilKurde"
 version = "2.0"
 
 repositories {
-    mavenCentral()
+	mavenCentral()
 }
 
 dependencies {
-    implementation("com.jessecorbett:diskord-bot:5.2.0")
+	implementation("com.jessecorbett:diskord-bot:5.2.0")
 }
 
 tasks.withType<KotlinCompile> {
-    kotlinOptions.freeCompilerArgs = listOf("-Xcontext-receivers")
+	kotlinOptions.freeCompilerArgs = listOf("-Xcontext-receivers")
 }
 
 application {
-    mainClass.set("BotKt")
+	mainClass.set("BotKt")
 }
 
-tasks.withType(ShadowJar::class.java){
-    minimize()
-    archiveClassifier.set("")
+tasks.withType(ShadowJar::class.java) {
+	archiveClassifier.set("")
 }
