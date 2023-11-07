3ed4480418d0089bb77518ecbada499bfb0c67ae
KamilKurde <kamilbak113@gmail.com>
2023-11-07 16:36:49 +0100

    Bump version

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 }
 
 group = "com.github.KamilKurde"
-version = "2.0"
+version = "2.1"
 
 repositories {
 	mavenCentral()
--- src/main/kotlin/util/Edit.kt
+++ src/main/kotlin/util/Edit.kt
@@ -5,7 +5,6 @@ import com.jessecorbett.diskord.api.channel.MessageEdit
 import com.jessecorbett.diskord.api.common.Message
 import com.jessecorbett.diskord.bot.BotContext
 
-
 context (BotContext)
 suspend fun Message.edit(block: Embed.() -> Unit) = edit(
 	MessageEdit(
